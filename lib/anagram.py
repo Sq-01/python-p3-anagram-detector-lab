8f64a0fd6e28f286d1596406ca0a19161fde9a27
Sadiq <sadiq.said@student.moringaschool.com>
2023-10-23 21:53:56 +0300

    Completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        anagrams = []
+        normalized_word = self.normalize(self.word)
+        for word in word_list:
+            if normalized_word == self.normalize(word) and self.word.lower() != word.lower():
+                anagrams.append(word)
+        return anagrams
+
+    @staticmethod
+    def normalize(word):
+        return sorted(word.lower())
